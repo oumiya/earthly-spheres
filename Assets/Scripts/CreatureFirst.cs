273042d8e8feb512aa8a2d29885564922b18cab2
nekosuzu <itsurou@nn.iij4u.or.jp>
2018-06-23 17:32:25 +0900

    最初の生き物の仮実装

---
+++ Assets/Scripts/CreatureFirst.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureFirst : MonoBehaviour {
+    Vector3 Aim_vector;
+
+	// Use this for initialization
+	void Start () {
+        Aim_vector = new Vector3(5, 0, 0);
+
+    }
+    // Update is called once per frame
+    float y;
+    void Update () {
+        y += Time.deltaTime * ((Random.value-0.5f)*5);
+        if (y > 3.0f) y = 3.0f;
+        if (y < -3.0f) y = -3.0f;
+        Aim_vector = Quaternion.Euler(0, y, 0) * Aim_vector;
+        Debug.Log("Aim_vector:" + Aim_vector);
+        GetComponent<Rigidbody>().velocity = Aim_vector * 1f;
+    }
+}
